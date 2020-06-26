96d3d3c9146fbed3d6747f29179e72208ebeb769
Ashwin Chatterji <ashwinath@hotmail.com>
2020-06-26 10:06:20 +0000

    add ubuntu install

---
+++ style_points/install-dependencies-ubuntu.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Author: Ashwin Chatterji <ashwinath@hotmail.com>
+# Sets up Dotfiles for Arch Linux
+sudo apt update
+sudo apt install -y \
+    tmux \
+    zsh \
+    neovim \
+    automake \
+    pkg-config \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common \
+    make \
+    build-essential \
+    gcc
+
+# Install Docker
+sudo apt remove docker docker-engine docker.io containerd runc
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt -y install docker-ce docker-ce-cli containerd.io
+
+# Install universal ctags
+git clone https://github.com/universal-ctags/ctags.git
+pushd ctags
+./autogen.sh 
+./configure
+make
+sudo make install
+popd
+rm -rf ctags
--- style_points/setup-dotfiles.sh
+++ style_points/setup-dotfiles.sh
@@ -1,50 +1,11 @@
 #!/bin/bash
 # Author: Ashwin Chatterji <ashwinath@hotmail.com>
-# Sets up Dotfiles for Arch Linux
-sudo pacman -Syu --noconfirm
-sudo pacman -S --noconfirm \
-    telegram-desktop \
-    neofetch \
-    ttf-joypixels \
-    xclip \
-    openssh \
-    spotify \
-    tree \
-    man \
-    tmux \
-    python-pip \
-    sysstat \
-    git \
-    wget \
-    curl \
-    i3-gaps \
-    i3blocks \
-    lightdm \
-    xorg-server \
-    xorg-xinit \
-    feh \
-    firefox \
-    zsh \
-    neovim \
-    alacritty \
-    rofi \
-    docker
+# Sets up Dotfiles
 
 # Docker
 sudo usermod -aG docker $(whoami)
 sudo systemctl start docker
 sudo systemctl enable docker
-
-# Compile yay
-git clone https://aur.archlinux.org/yay.git --depth=1
-pushd yay
-makepkg -si
-popd
-rm -rf yay
-
-yay -S --answerdiff=None \
-    lightdm-slick-greeter \
-    universal-ctags-git
 
 # Fonts
 git clone https://github.com/ryanoasis/nerd-fonts --depth=1
