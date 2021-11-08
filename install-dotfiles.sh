60934cf8f0b69a5b6399f37325addcde2181e771
Ashwin <ashwinath@hotmail.com>
2021-11-08 22:49:11 +0800

    uncomment

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -11,10 +11,10 @@ RUNZSH=no sh install.sh
 # Neovim
 mkdir -p ${HOME}/.config/nvim/
 ln -sf ${PWD}/nvim/init.vim ${HOME}/.config/nvim/init.vim
-#curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    #https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-#nvim +'PlugInstall --sync' +qa
-#pip install neovim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+nvim +'PlugInstall --sync' +qa
+pip install neovim
 
 # Alacritty
 mkdir -p ${HOME}/.config/alacritty/
